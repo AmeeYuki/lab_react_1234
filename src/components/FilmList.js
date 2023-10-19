1c59652a668c6f67d16eaf6c16b116994662add5
AmeeYuki <yukinooame@gmail.com>
2023-10-19 11:05:58 +0700

    lab21.1

--- src/components/FilmList.js
+++ src/components/FilmList.js
@@ -1,46 +1,94 @@
 import React from "react";
-import {
-  List,
-  ListItem,
-  ListItemText,
-  Typography,
-  Paper,
-  Grid,
-  Avatar,
-  Container,
-  Card,
-} from "@mui/material";
+import { Paper, Container, Card } from "@mui/material";
 import { FilmData } from "../shared/ListOfFilms";
-import { Image } from "@mui/icons-material";
 import { Row } from "react-materialize";
+import { useState } from "react";
 
 function FilmList() {
+  const [film, setFilm] = useState([]);
   return (
-    <div>
+    <div className="mt-4 mb-4">
       <Container>
-        <h4>List of Film:</h4> <hr />
+        <h4>
+          <b>List of Film</b>:
+        </h4>{" "}
+        <hr />
         <Paper>
           <div className="row">
             {FilmData.map((film) => (
-              <div className="col-md-6 mb-4" key={film.id}>
+              <div className="col-md-6" key={film.id}>
                 <Card>
                   <Row>
-                    <div className="col-7 col-md-7">
+                    <div className="col-5 col-md-7">
                       <img
                         src={film.image}
                         alt={film.title}
                         className="img-film"
                       />
                     </div>
-                    <div className=" col-5 col-md-5">
-                      <h5>Title: {film.title} </h5>
-                      <p>Year: {film.year}</p>
-                      <p>Nation: {film.nation}</p>
+                    <div className=" col-7 col-md-5">
+                      <div className="title">
+                        <h5>
+                          <b>Name:</b> {film.title}{" "}
+                        </h5>
+                        <p>
+                          <b>Year:</b> {film.year}
+                        </p>
+                        <p>
+                          <b>Nation</b>: {film.nation}
+                        </p>
+                      </div>
+                      <button
+                        className="btn btn-success"
+                        onClick={() => {
+                          setFilm(film);
+                        }}
+                      >
+                        <a
+                          style={{ color: "white", fontSize: "15px" }}
+                          href="#popup1"
+                          id="openPopUp"
+                        >
+                          <b>View Detail</b>
+                        </a>
+                      </button>
                     </div>
                   </Row>
                 </Card>
               </div>
             ))}
+            <Card>
+              <div id="popup1" className="overlay ">
+                <div className="popup container mt-5">
+                  <div className="row">
+                    <div className="col-4">
+                      <img src={film.image} alt={film.title} />
+                    </div>
+
+                    <div className="col-8">
+                      <h4>{film.title}</h4>
+
+                      <p style={{ fontSize: "15px" }} className="content">
+                        {film.info}
+                      </p>
+
+                      <a href="#">
+                        <button
+                          className="close btn-danger"
+                          style={{
+                            backgroundColor: "red",
+                            fontSize: "20px",
+                            color: "white",
+                          }}
+                        >
+                          Close
+                        </button>
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Card>
           </div>
         </Paper>
       </Container>
--- src/shared/ListOfFilms.js
+++ src/shared/ListOfFilms.js
@@ -16,6 +16,7 @@ export const FilmData = [
     title: "Dragon Ball",
     year: "1977",
     nation: "JAPAN",
+    info: "Dragon Ball is a Japanese media franchise created by Akira Toriyama in 1984. The initial manga, written and illustrated by Toriyama, was serialized in Weekly Shōnen Jump from 1984 to 1995, with the 519 individual chapters collected into 42 tankōbon volumes by its publisher Shueisha.",
   },
   {
     id: 2,
@@ -23,6 +24,7 @@ export const FilmData = [
     title: "365 DAYS",
     year: "2009",
     nation: "USA",
+    info: "The life of Laura, a simple sales director, changes when she takes a trip to Sicily but gets kidnapped by Massimo, a mafia member. Massimo keeps her hostage for 365 days to make her fall for him.",
   },
   {
     id: 3,
@@ -30,6 +32,7 @@ export const FilmData = [
     title: "IT Chú Hề Ma Quái",
     year: "2003",
     nation: "New Zealand",
+    info: "Chú hề ma quái là một bộ phim kinh dị siêu nhiên Mỹ ra mắt năm 2017 của đạo diễn Andy Muschietti. Đây là phần đầu tiên trong kế hoạch sản xuất loạt phim It hai phần dựa trên cuốn tiểu thuyết cùng tên của nhà văn Stephen King. Nhóm biên kịch của phim gồm có Chase Palmer, Cary Fukunaga và Gary Dauberman.",
   },
   {
     id: 4,
@@ -37,6 +40,7 @@ export const FilmData = [
     title: "Star Wars",
     year: "1997",
     nation: "USA",
+    info: "Star Wars is an American epic space opera media franchise created by George Lucas, which began with the eponymous 1977 film and quickly became a worldwide pop culture phenomenon.",
   },
   {
     id: 5,
@@ -44,6 +48,7 @@ export const FilmData = [
     title: "Conan Movie 26",
     year: "1993",
     nation: "USA",
+    info: "Thám tử lừng danh Conan: Tàu ngầm sắt màu đen là phim điện ảnh anime trinh thám năm 2023 của Nhật Bản dựa trên nguyên tác là bộ manga Thám tử lừng danh Conan của họa sĩ Aoyama Gōshō. Phim do Yuzuru Tachikawa làm đạo diễn, dựa trên phần kịch bản do Takeharu Sakurai chấp bút.",
   },
   {
     id: 6,
@@ -51,6 +56,7 @@ export const FilmData = [
     title: "Đất Phương Nam",
     year: "2010",
     nation: "USA",
+    info: "Đất phương Nam là một bộ phim ngắn tập chính kịch phiêu lưu năm 1997 do Nguyễn Vinh Sơn làm đạo diễn kiêm viết kịch bản. Bộ phim dựa trên tiểu thuyết Đất rừng phương Nam của nhà văn Đoàn Giỏi, kể về hành trình tìm cha của một cậu bé ở vùng miền Tây Nam Bộ",
   },
   {
     id: 7,
@@ -58,6 +64,7 @@ export const FilmData = [
     title: "Doraemon Movie",
     year: "2008",
     nation: "USA",
+    info: "Doraemon is a fictional character in the Japanese manga and anime series of the same name created by Fujiko F. Fujio. Doraemon is a male robotic earless cat that travels back in time from the 22nd century to aid a preteen boy named Nobita.",
   },
   {
     id: 8,
@@ -65,6 +72,7 @@ export const FilmData = [
     title: "Mười Lời Nguyền",
     year: "1994",
     nation: "USA",
+    info: "Mười: Lời nguyền trở lại là phim điện ảnh kinh dị Việt Nam, ra mắt năm 2022. Phim do Hằng Trịnh đạo diễn, nhà sản xuất là hãng Silver Moonlight Entertainment. Dàn diễn viên chính trong phim gồm có: Chi Pu, Rima Thanh Vy, Hồng Ánh, Bình Minh, Anh Thư. Đây cũng là phần hai của tác phẩm cùng tên, ra mắt năm 2007.",
   },
   {
     id: 9,
@@ -72,6 +80,7 @@ export const FilmData = [
     title: "One Piece Film Red",
     year: "1994",
     nation: "USA",
+    info: "One Piece Film: Red là bộ phim hoạt hình anime của Nhật Bản thuộc thể loại kỳ ảo, hành động-phiêu lưu được sản xuất bởi Toei Animation. Đây là phần phim thứ mười lăm trong loạt phim điện ảnh của One Piece, dựa trên bộ truyện manga nổi tiếng cùng tên của tác giả Eiichiro Oda.",
   },
   {
     id: 10,
@@ -79,5 +88,6 @@ export const FilmData = [
     title: "Sailor Moon",
     year: "1992",
     nation: "Japan",
+    info: "Sailor Moon is a Japanese manga series written and illustrated by Naoko Takeuchi. It was originally serialized in Kodansha's shōjo manga magazine Nakayoshi from 1991 to 1997; the 52 individual chapters were published in 18 volumes.",
   },
 ];
